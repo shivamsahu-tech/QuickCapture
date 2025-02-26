d7b2b80ed9268852fc50b0786bdc78b8cc5f50f3
Shivam Sahu <shivamsahu258@outlook.com>
2025-02-26 19:53:43 +0530

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,26 @@ import FileUpload from "@/components/FileUpload";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Quick Capture",
-  description: "Capture your moments",
+export const metadata = {
+  title: "Quick Capture – Capture, Edit & Save Effortlessly",
+  description: "Quick Capture allows you to take notes, edit, and store them effortlessly. Now with profile image uploads & seamless UI!",
+  keywords: "Quick Capture, online notes, cloud notes, Markdown notes, Cloudinary upload, Next.js notes app",
+  openGraph: {
+    title: "Quick Capture – Capture, Edit & Save Effortlessly",
+    description: "Now with profile photo uploads, optimized UI, and smooth note management!",
+    url: "https://yourwebsite.com",
+    siteName: "Quick Capture",
+    images: [
+      {
+        url: "https://th.bing.com/th/id/OIP.wlVtctNEeskBNhpk2H2UFQHaHa?pid=ImgDet&w=474&h=474&rs=1", // Replace with your OG image
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
 };
+
 
 export default function RootLayout({
   children,
